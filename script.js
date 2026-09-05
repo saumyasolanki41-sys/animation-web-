5c68cce882b896f2aab8601a01892b3d44bf74ee
saumyasolanki41-sys <saumyasolanki41@gmail.com>
2026-09-05 10:01:43 +0530

    add particle class and drawing method

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const context= canvas.getcontext("2d");
 
 canvas.width= window.innerWidth;
 canvas.height= window.innerHeight;
+
+const particles=[];
+let hue=0;
+
+class particle{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.radius=Math.random()*6+2;
+        this.color=`hsl(${hue}, 100%, 50% )`;
+    }
+
+    draw(){
+        context.beginPath();
+        context.arc(
+            this.x,
+            this.y,
+            this.radius,
+            0,
+            Math.PI*2
+        );
+        context.fillStyle=this.color;
+        context.fill();
+    }
+}
